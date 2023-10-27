1bdc203984058c31db80dacc14ee2e466a6cb5dd
Kakaroco88 <aswadh250@gmail.com>
2023-10-27 21:57:31 +0700

    edit terbaru index dan tambah file contact

--- src/pages/src/pages/contact.js
+++ src/pages/src/pages/contact.js
@@ -5,7 +5,8 @@ return (
 	<div>
 		<h1>Contact</h1>
 		<p>Saya bisa dihubungi via @kakaroco_maps on instagram</p>
-		<p>Saya bisa dihubungi via <a href="https://instagram.com/kakaroco_maps" target="_blank">@kakaroco_maps</a> di instagram!</p>
+		<p>Saya bisa dihubungi via <a href="https://instagram.com/kakaroco_maps"
+			target="_blank">@kakaroco_maps</a> di instagram!</p>
 	</div>
 )
 }
