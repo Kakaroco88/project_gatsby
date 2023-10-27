15bea9c2691f251615c992fc3bf6c723e01e04d1
Kakaroco88 <aswadh250@gmail.com>
2023-10-27 20:04:31 +0700

    upload project ke github

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const IndexPage = () => {
+	return (
+		<div>	
+			<h1>Mata Kuliah Web Hosting Semester 7</h1>
+			<h2>Ricko Alhabsi, Semoga sehat selalu ya..</h2>
+			<h3> Mabda atau ideologi adalah sebuah paham  atau ideologi yang akan menghasilkan sebuah peraturan.
+			Pertama adalah paham islami yaitu memiliki paham atau pedoman hidupnya menerapkan isi kitabullah dan sunnah.
+			Kedua adalah paham komunis yaitu hidup  secara sosialis dimana tidak ada peran Tuhan  di dalam kehidupan.
+			Ketiga adalah paham kapitalisme yaitu paham dimana yang kaya menguasai kehidupan, sehingga muncul paham sekulerisme 
+			yang menghasilkan 3 point. Pertama bebas berpendapat, bebas berekspresi dan bebas memiliki.</h3>
+			
+		</div>
+	)
+}
+
+export default IndexPage
