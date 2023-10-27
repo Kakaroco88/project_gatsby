cadd062ee6ab217e10cef3c91dc5d0ce5f2f96f9
Kakaroco88 <aswadh250@gmail.com>
2023-10-27 21:16:09 +0700

    edit index dan tambah file contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ const IndexPage = () => {
 		<div>	
 			<h1>Mata Kuliah Web Hosting Semester 7</h1>
 			<h2>Ricko Alhabsi, Semoga sehat selalu ya..</h2>
-			<h3> Mabda atau ideologi adalah sebuah paham  atau ideologi yang akan menghasilkan sebuah peraturan.
+			<p> Mabda atau ideologi adalah sebuah paham  atau ideologi yang akan menghasilkan sebuah peraturan.
 			Pertama adalah paham islami yaitu memiliki paham atau pedoman hidupnya menerapkan isi kitabullah dan sunnah.
 			Kedua adalah paham komunis yaitu hidup  secara sosialis dimana tidak ada peran Tuhan  di dalam kehidupan.
 			Ketiga adalah paham kapitalisme yaitu paham dimana yang kaya menguasai kehidupan, sehingga muncul paham sekulerisme 
-			yang menghasilkan 3 point. Pertama bebas berpendapat, bebas berekspresi dan bebas memiliki.</h3>
-			
+			yang menghasilkan 3 point. Pertama bebas berpendapat, bebas berekspresi dan bebas memiliki.</p>
+			<p>Kontak saya <a href="/contact">Hub Saya</a></p>
+			<Mau jadi developer <Link to="/contact">Klik Mau</Link></p>
 		</div>
 	)
 }
